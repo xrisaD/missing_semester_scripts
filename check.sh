d1675da074728c4a0835a5833d667df29466d319
xrisaD <xrdikonimaki@hotmail.com>
2021-03-13 09:22:12 +0200

    added description to check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
  #!/bin/bash
+ # a bash script that runs the given script until it fails and captures its standard output and error streams to files and prints everything at the end (report how many runs it took for the script to fail).
  count=0
  $1 1>stdout.txt 2>stderr
  while [ $? -eq 0 ]; do
